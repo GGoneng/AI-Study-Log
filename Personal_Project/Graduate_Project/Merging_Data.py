6d96e31f8ee657eb1c3a5748e9066e4d73405899
GGoneng <zxzx0031@naver.com>
2025-05-22 12:24:31 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Personal_Project/Graduate_Project/Data_Preprocess.ipynb
    	modified:   Personal_Project/Graduate_Project/Fall_Detection.ipynb
    	new file:   Personal_Project/Graduate_Project/Final_model/model_script.pt
    	modified:   Personal_Project/Graduate_Project/Merging_Data.py

--- Personal_Project/Graduate_Project/Merging_Data.py
+++ Personal_Project/Graduate_Project/Merging_Data.py
@@ -3,7 +3,7 @@ import re
 import os
 
 # CSV 파일들이 있는 폴더 경로
-folder_path = './new_10fps_Dataset'  # 예: './data'
+folder_path = './10fps_Dataset'  # 예: './data'
 csv_files = sorted(
     [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.csv')],
     key=lambda x: int(re.search(r'\d+', os.path.basename(x)).group())
@@ -20,6 +20,6 @@ for idx, file in enumerate(csv_files):
 # 병합
 merged_df = pd.concat(df_list, ignore_index=True)
 
-merged_df.to_csv("merged_data.csv", index = False)
+merged_df.to_csv("10fps_merged_data.csv", index = False)
 
 print(merged_df['source_index'].value_counts().sort_index())
